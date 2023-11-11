b1912c0745bf48651db3f80100825a27f1ff44dd
Pavel Moiseenko <moiseenko.pavel@hf-solution>
2023-11-11 10:00:22 +0300

    update tests

--- src/test/java/com/palindrom/service/impl/StaticServiceImplTest.java
+++ src/test/java/com/palindrom/service/impl/StaticServiceImplTest.java
@@ -4,6 +4,7 @@ import com.palindrom.service.StaticService;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StaticServiceImplTest {
@@ -15,5 +16,11 @@ class StaticServiceImplTest {
             "А роза упала на лапу Азора", "шабаш", "комок"})
     void isPalindrome(String str) {
         assertTrue(staticService.isPalindrome(str, 0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "is not palindrome"})
+    void isNotPalindrome(String str) {
+        assertFalse(staticService.isPalindrome(str, 0));
     }
 }
