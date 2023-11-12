1cc27fb6a84f3cb36e634d10945967cad08fe203
Pavel Moiseenko <moiseenko.pavel@hf-solution>
2023-11-12 21:25:41 +0300

    refactoring

--- src/main/java/com/palindrome/Main.java
+++ src/main/java/com/palindrome/Main.java
@@ -1,12 +1,20 @@
 package com.palindrome;
 
+import com.palindrome.repository.UserRepository;
 import com.palindrome.service.GameService;
 import com.palindrome.service.impl.GameServiceImpl;
+import com.palindrome.service.impl.PalindromeServiceImpl;
+import com.palindrome.service.impl.StatisticServiceImpl;
+import com.palindrome.service.impl.UserServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        GameService gameService = new GameServiceImpl();
+        UserServiceImpl userService = new UserServiceImpl(new UserRepository());
+        StatisticServiceImpl statisticService = new StatisticServiceImpl(userService);
+        PalindromeServiceImpl palindromeService = new PalindromeServiceImpl();
+
+        GameService gameService = new GameServiceImpl(userService, statisticService, palindromeService);
         gameService.startGame();
     }
 }
